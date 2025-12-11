6548f4b78f3a39ba8f6f7cab5e3939f595a440be
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-12-11 16:14:44 +0530

    generate & refresh token

--- youtube/models/userModels.js
+++ youtube/models/userModels.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
-
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const userSchema = new Schema({
   username: {
     type:String,
@@ -43,6 +44,35 @@ const userSchema = new Schema({
     type:String
   }
 },{timestamps:true})
+
+userSchema.pre("save", async function(next){
+  if(!this.modified("password")) return next()
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  // short live token
+  jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username: this.username,
+    fullname: this.fullname
+  },process.env.ACCESS_TOKEN_SECRET,
+  {expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  // short live token
+  jwt.sign({
+    _id:this._id,
+  },process.env.REFRESH_TOKEN_SECRET,
+  {expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
+}
 
 const User = mongoose.model("user", userSchema)
 
